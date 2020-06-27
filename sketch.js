96f50a1c46b0e88a0874e9716a7d84593a991445
jkutkut <jorgeregonzalez@gmail.com>
2020-06-27 17:06:32 +0200

    Added sliders Added texboxes to implement any fourier series Added labels to know the meaning of al the inputs Added btn to update the the function
    Logic to make this things work missing

--- sketch.js
+++ sketch.js
@@ -4,57 +4,93 @@
 // https://thecodingtrain.com/CodingChallenges/125-fourier-series.html
 
 let time = 0;
-let wave = [];
-let path = [];
+let wave = []; // the actual respresentation. Only the y values (f(x)) are stored
 
-let slider;
+let nSlider;
+
+let xCoord = function(ele){
+    return ele.x + ele.width;
+    // return - width / 2 + slider.x * 2 + slider.width - 30;
+}
+let yCoord = function(slider){
+    return slider.y
+    // return - height / 2 + slider.y
+}
 
 function setup() {
-  createCanvas(600, 400);
-  slider = createSlider(1, 50, 5);
+  createCanvas(650, 500);
+  // Sliders
+  nSlider = createSlider(1, 50, 10);
+  nSlider.position(10, height - 50);
+  eSlider = createSlider(20, 190, 76);
+  eSlider.position(10, height - 20);
+  
+  // Inputs
+  a0Input = createInput()
+  a0Input.position(nSlider.x + 230, nSlider.y)
+
+  anInput = createInput()
+  anInput.position(eSlider.x + 230, eSlider.y)
+
+  bnInput = createInput()
+  bnInput.position(a0Input.x + 230, nSlider.y)
+
+  //Button:
+  updateBtn = createButton("Update")
+  updateBtn.position(anInput.x + 230, eSlider.y)
+  updateBtn.size(bnInput.width, bnInput.height)
+
+  textSize(15);
+  textAlign(LEFT, CENTER);
 }
 
 function draw() {
   background(0);
-  translate(150, 200);
+  fill(255)
+  text('n', xCoord(nSlider), nSlider.y);
+  text('Scale', xCoord(eSlider), eSlider.y);
+  text('a0', xCoord(nSlider) + 60, nSlider.y);
+  text('an', xCoord(eSlider) + 60, eSlider.y);
+  text('bn', xCoord(a0Input) + 10, nSlider.y);
 
-  let x = 0;
-  let y = 0;
 
-  for (let i = 0; i < slider.value(); i++) {
-    let prevx = x;
-    let prevy = y;
+  translate(eSlider.value() + 100, height / 2);
+
+  let x = 0, y = 0; // coordinates of the center of the actual circle
+
+  for (let i = 0; i < nSlider.value(); i++) {
+    let prevx = x, prevy = y; // Coordinates of the previous circle's center
 
     let n = i * 2 + 1;
-    let radius = 75 * (4 / (n * PI));
+    let radius = eSlider.value() * (4 / (n * PI));
     x += radius * cos(n * time);
     y += radius * sin(n * time);
 
-    stroke(255, 100);
-    noFill();
+    stroke(255, 100); // make the line white with some alpha
+    noFill(); // empty circle
     ellipse(prevx, prevy, radius * 2);
 
-    //fill(255);
-    stroke(255);
-    line(prevx, prevy, x, y);
-    //ellipse(x, y, 8);
+    stroke(255); // make the line pure white
+    line(prevx, prevy, x, y); // Draw the radius of the circle
   }
+  
   wave.unshift(y);
 
 
   translate(200, 0);
   line(x - 200, y, 0, wave[0]);
-  beginShape();
+  beginShape(); // Start drawing the wave
   noFill();
   for (let i = 0; i < wave.length; i++) {
     vertex(i, wave[i]);
   }
   endShape();
 
+//   time += 0.01;
   time += 0.05;
 
 
-  if (wave.length > 250) {
+  if (wave.length > 250) { // No need to store all the wave, remove the end when it gets big
     wave.pop();
   }
 }
